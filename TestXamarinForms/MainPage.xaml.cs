79b8e7d628d5c554f253b86dec00de46f5128829
Pierre Chalamet <pierre@chalamet.net>
2018-10-02 11:05:04 +0200

    initial commit on mac

---
+++ TestXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestXamarinForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
